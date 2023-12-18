dab6d825263b61e5e0e5789616c3adc5b70d2a3f
mary-martin <mary.martin@colorado.edu>
2023-12-17 17:56:57 -0700

    combinne redundant code, remove obsolete comments

--- relations/grounding.py
+++ relations/grounding.py
@@ -18,27 +18,17 @@ class SpatialPredictor(object):
             "right": "left",
             "front": "behind",
             "left": "right",
-            "behind": "front",
+            "behind": "front"
         }
         self.relation_constraints = {
             "left": [1, 1],
             "right": [1, -1],
             "front": [0, -1],
-            "behind": [0, 1],
+            "behind": [0, 1]
         }
 
     # Function to perform initial grounding of object mentions in the scene
     def initial_grounding(self, mentions):
-        """
-        Perform initial grounding of object mentions in the scene.
-
-        Parameters:
-        - mentions (dict): Dictionary containing mentions in the scene.
-        - entities (list): List of entities in the scene.
-
-        Returns:
-        - tuple: A tuple containing dictionaries of grounded and ungrounded objects.
-        """
         obj_mentions = mentions["objects"]
         grounded = defaultdict(list)
         ungrounded = []
@@ -74,25 +64,15 @@ class SpatialPredictor(object):
         return grounded
 
     def relate(self, mentions):
-        """
-        Establish spatial relations between objects in the scene.
-
-        Parameters:
-        - mentions (dict): Dictionary containing relations in the scene.
-        - grounded (dict): Dictionary containing grounded objects.
-        - scene_info (SceneInfo): Information about the scene.
-
-        Returns:
-        - dict: Dictionary containing qualifying pairs of objects based on spatial relations.
-        """
         self.grounded = self.initial_grounding(mentions)
         true_groundings = self.grounded.keys()
-        self.true_nodes = reduce(set.union, (set(lst) for lst in self.grounded.values()))
+        self.true_nodes = reduce(
+            set.union, (set(lst) for lst in self.grounded.values())
+        )
         print(self.true_nodes)
         relations = mentions["relations"]
         self.candidates = self.grounded
 
-        #
         # Loop through relations and find candidates for o2 (goal object) in each relation mention
         while relations:
             ug_relations = []
@@ -102,7 +82,7 @@ class SpatialPredictor(object):
                 relation_label = relation["label"]
                 o1_exists = o1 in self.candidates.keys()
                 o2_exists = o2 in self.candidates.keys()
-                
+
                 if "o3" in relation.keys():
                     if not self.decompose(relation):
                         ug_relations.append(relation)
@@ -125,19 +105,14 @@ class SpatialPredictor(object):
             print(self.candidates)
 
         return ug_relations, self.candidate_graph
-    
+
     def match_candidates(self, o1, o2, relation_label):
         o1_entities = self.candidates[o1]
         for obj in o1_entities:
-            candidate_obj, matched = self.evaluate_pairs(
-                obj, relation_label
-            )
+            candidate_obj, matched = self.evaluate_pairs(obj, relation_label)
             if matched:
                 self.candidates[o2] = list(
-                    reduce(
-                        set.union,
-                        map(set, [self.candidates[o2], candidate_obj])
-                    )
+                    reduce(set.union, map(set, [self.candidates[o2], candidate_obj]))
                 )
 
     def clear_graph(self):
@@ -160,21 +135,11 @@ class SpatialPredictor(object):
 
     # Function to evaluate pairs of objects based on a spatial relation
     def evaluate_pairs(self, object1, relation_label, base_score=0):
-        """
-        Evaluate pairs of objects based on a spatial relation.
-
-        Parameters:
-        - object1 (Entity): The first object in the relation.
-        - relation_label (str): The spatial relation label.
-        - scene_info (SceneInfo): Information about the scene.
-
-        Returns:
-        - dict: Dictionary of matched objects and their offsets.
-        """
         scene_obj_idx = object1
         relation_direction = self.relation_constraints[relation_label]
         obj_specific_pairs = self.offsets.get_obj_offsets(scene_obj_idx)
         matched_objs = []
+        scores = []
         matched = False
 
         for paired_obj, offset in obj_specific_pairs.items():
@@ -185,27 +150,28 @@ class SpatialPredictor(object):
                 if paired_obj not in self.candidate_graph.nodes():
                     self.candidate_graph.add_node(paired_obj)
                 score = self.score_relations(offset, dim)
-                self.candidate_graph.add_edge(scene_obj_idx, paired_obj, weight=score)
+                scores.append(score)
+                # self.candidate_graph.add_edge(scene_obj_idx, paired_obj, weight=score)
                 # offset['score'] = score
                 matched_objs.append(paired_obj)
                 if not matched:
                     matched = True
 
+        magnitude, norm_scores = self.calculate_norm(scores)
+        for match, final_score in zip(matched_objs, norm_scores):
+            self.candidate_graph.add_edge(scene_obj_idx, match, weight=final_score)
+
         return matched_objs, matched
 
     # Function to score spatial relations based on distance and ratio
-    def score_relations(self, offset, dim, dist_scaling=-1, ratio_scaling=1):
-
+    def score_relations(self, offset, dim, dist_scaling=-0.05, ratio_scaling=1):
         xyz_offsets = abs(np.array(offset["xyz_offsets"]))
         xy_offsets = [xyz_offsets[0], xyz_offsets[1]]
         # direction_distance = xyz_offsets[dim]
         magnitude, vec_norm = self.calculate_norm(xy_offsets)
         norm_offset = vec_norm[dim]
 
-        score = (
-            dist_scaling * magnitude
-            + ratio_scaling * norm_offset
-        )
+        score = dist_scaling * magnitude + ratio_scaling * norm_offset
 
         return score
 
@@ -220,42 +186,37 @@ class SpatialPredictor(object):
 
     def decompose(self, relation, weight=-1):
         decomposable = False
-        o1 = relation["o1"]
-        o2 = relation["o2"]
-        o3 = relation["o3"]
+        o1, o2, o3 = relation["o1"], relation["o2"], relation["o3"]
         r1 = {"o1": o2, "o2": o1, "label": "opposing"}
         r2 = {"o1": o3, "o1": o1, "label": "opposing"}
         op_relations = [r1, r2]
 
-        if o2 and o3 in self.grounded.keys():
+        if all(obj in self.candidates for obj in [o2, o3]):
             decomposable = True
-            o2_idx = self.grounded[o2][0]
-            o3_idx = self.grounded[o3][0]
-            o2_offsets = self.offsets.get_obj_offsets(o2_idx)
-            o3_offsets = self.offsets.get_obj_offsets(o3_idx)
-            for candidate in o2_offsets.keys():
-                if candidate in o3_offsets.keys():
-                    offset1 = o2_offsets[candidate]
-                    offset2 = o3_offsets[candidate]
-                    vector1 = offset1["direction"]
-                    vector2 = offset2["direction"]
-                    target_loc = np.array(self.obj_locations[candidate])
-                    o2_loc = np.array(self.obj_locations[o2])
-                    o3_loc = np.array(self.obj_locations[o3])
-                    score = self.cos_angle(vector1, vector2)
-                    if score <= 0.0:
-                        score = weight * self.euclidean_distance(
-                            target_loc, o2_loc, o3_loc
-                        )
-                        print(score)
-                        if candidate not in self.candidate_graph.nodes():
-                            self.candidate_graph.add_node(candidate)
-                        self.candidate_graph.add_edge(
-                            o2_idx, candidate, weight=score / 2
-                        )
-                        self.candidate_graph.add_edge(
-                            o3_idx, candidate, weight=score / 2
-                        )
+            o2_idx, o3_idx = self.candidates[o2][0], self.candidates[o3][0]
+            o2_offsets, o3_offsets = self.offsets.get_obj_offsets(
+                o2_idx
+            ), self.offsets.get_obj_offsets(o3_idx)
+            for candidate in set(o2_offsets) & set(o3_offsets):
+                offset1, offset2 = o2_offsets[candidate], o3_offsets[candidate]
+                vector1, vector2 = offset1["direction"], offset2["direction"]
+                target_loc, o2_loc, o3_loc = np.array(self.obj_locations[candidate]), np.array(self.obj_locations[o2]), np.array(self.obj_locations[o3])
+
+                angle_eval = self.cos_angle(vector1, vector2)
+                if angle_eval <= 0.0:
+                    midpoint = (vector1 + vector2) / 2
+                    score = weight * self.euclidean_distance(
+                        target_loc, o2_loc, o3_loc
+                    )
+                    print(score)
+                    if candidate not in self.candidate_graph.nodes():
+                        self.candidate_graph.add_node(candidate)
+                    self.candidate_graph.add_edge(
+                        o2_idx, candidate, weight=score / 2
+                    )
+                    self.candidate_graph.add_edge(
+                        o3_idx, candidate, weight=score / 2
+                    )
 
         return decomposable
 
@@ -283,5 +244,5 @@ class SpatialPredictor(object):
         magnitude = np.linalg.norm(vector)
         # Normalize the vector
         normalized_vector = vector / magnitude
-        
+
         return magnitude, normalized_vector
